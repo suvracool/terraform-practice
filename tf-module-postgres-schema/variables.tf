cc8651d47d5f91062424c21d777de1f11bf8ea0b
suvracool <45148250+suvracool@users.noreply.github.com>
2023-07-25 13:25:42 +0200

    Create variables.tf

---
+++ tf-module-postgres-schema/variables.tf
@@ -0,0 +1,12 @@
+variable "database_name" {
+  description = "The database name."
+  type        = string
+  default     = null
+}
+
+variable "schema_name" {
+  description = "The schema name."
+  type        = string
+  default     = null
+}
+
